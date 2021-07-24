ff96fc9862470917824cfd53ca8071bf8a3752bc
Cian <cian.omondi@student.moringaschool.com>
2021-07-24 12:18:45 +0300

    changed path on the import,added a blueprint property

--- src/app/git/git.component.ts
+++ src/app/git/git.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../user-service/user.service';
-import { User } from '../user';
+import { User } from '../models/user';
 import { Repoview } from '../models/repoview';
 
 @Component({
--- src/app/models/repoview.ts
+++ src/app/models/repoview.ts
@@ -1,4 +1,9 @@
 export class Repoview {
 
-    constructor(public name: string, public id:number, public full_name:string,public forks:number){}
+    constructor(
+        public name: string,
+        public id: number,
+        public full_name: string,
+        public forks: number,
+        public description: string) { }
 }
